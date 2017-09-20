10c095ff6eb5ba35ce2b3093bb49fdf036b457ae
Charles Thompson <thompsoncharles@live.com>
2017-09-20 17:13:29 +0000

    Added Person base class. EIT and Fellow inherit from base Person class. Fellow uses super().__init__ to define new field

--- modeling_mest.py
+++ modeling_mest.py
@@ -1,8 +1,34 @@
+class Person:
+    def __init__(self):
+        self.name = str(input('Enter your name: '))
+        self.nationality = str(input('Enter your nationality: '))
+        
+
+class EITs(Person):
+    def __init__(self, name = "Charles", nationality = "Ghanaian"):
+        super().__init__(name, nationality)
+        
+    def recite_fun_fact(self, fun_fact = "Likes dancing"):
+        self.fun_fact = fun_fact
+        print(self.fun_fact)
+
+
+class Fellows(Person):
+    def __init__(self, name = "Francis", nationality = "Ghanaian", happiness_level = "0"):
+        super().__init__(name, nationality)
+        self.happiness_level = happiness_level
+
+    def eat(self):
+        self.happiness_level = self.happiness_level + 1
+
+    def teach(self):
+        self.happiness_level = self.happiness_level - 1
+
+        
 class School:
     def __init__(self):
         fellows = Fellows()
         eits = EITs()
-
 
     def recruit(self):
         
@@ -16,34 +42,9 @@ class School:
                 eit = eit_details.split(",")
                 if eit[1].strip() not in recruit_countries:
                     print ("Hello {}, \nMEST isn't recruiting from {} yet."
-                        "\nThanks\n".format(eit[0], eit[1]))
+                        "\nThanks.\n".format(eit[0], eit[1].strip()))
 
     
-
-
-class EITs:
-    def __init__(self, name = "Charles", nationality = "Ghanaian"):
-        self.name = name
-        self.nationality = nationality
-        
-    def recite_fun_fact(self, fun_fact = "Likes dancing"):
-        self.fun_fact = fun_fact
-        print(self.fun_fact)
-
-
-
-class Fellows:
-    def __init__(self, name = "Francis", nationality = "Ghanaian", happiness_level = "0"):
-        self.name = name
-        self.nationality = nationality
-        self.happiness_level = happiness_level
-
-    def eat(self):
-        self.happiness_level = self.happiness_level + 1
-
-    def teach(self):
-        self.happiness_level = self.happiness_level - 1
-
 if __name__ == '__main__':
     start_school = School()
     
